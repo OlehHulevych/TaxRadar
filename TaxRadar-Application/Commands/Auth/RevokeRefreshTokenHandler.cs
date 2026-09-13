43232149acd58c3ec41c1b22a172b38774d6a5b2
Oleh_Hulevych <oleggulevic8@gmail.com>
2026-09-13 15:47:13 +0200

    added revoking refresh tokens

---
+++ TaxRadar-Application/Commands/Auth/RevokeRefreshTokenHandler.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using Tax_Radar_Domain.Entities;
+using TaxRadar_Application.Exceptions;
+using TaxRadar_Application.Interfaces;
+using TaxRadar_Application.Queries.Auth;
+
+namespace TaxRadar_Application.Commands.Auth;
+
+public class RevokeRefreshTokenHandler(IRefreshTokenRepository repository):IRequestHandler<RevokeRefreshTokenQuery>
+{
+    public async Task Handle(RevokeRefreshTokenQuery request, CancellationToken cancellationToken)
+    {
+        var refreshToken = await repository.GetRefreshTokenByHash(request.RefreshToken, cancellationToken);
+        if (refreshToken == null) throw new NotFoundException(nameof(RefreshToken), request.RefreshToken);
+        refreshToken.Revoke();
+        await repository.SaveChangesAsync(cancellationToken);
+    }
+}
---
+++ TaxRadar-Application/Queries/Auth/RevokeRefreshTokenQuery.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+
+namespace TaxRadar_Application.Queries.Auth;
+
+public record RevokeRefreshTokenQuery(string RefreshToken):IRequest;
