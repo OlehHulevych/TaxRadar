25e093367fa9ae944a898e7b07618ccb338262cd
Oleh_Hulevych <oleggulevic8@gmail.com>
2026-09-13 15:23:12 +0200

    craete authentication handler via refresh and access tokens

---
+++ TaxRadar-Application/Commands/Auth/RefreshTokenCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using Tax_Radar_Domain.Entities;
+using TaxRadar_Application.DTOs.Auth;
+using TaxRadar_Application.Exceptions;
+using TaxRadar_Application.Interfaces;
+using TaxRadar_Application.Queries.Auth;
+
+namespace TaxRadar_Application.Commands.Auth;
+
+public class RefreshTokenCommandHandler( IJwtTokenService jwtTokenService, IRefreshTokenRepository refreshTokenRepository, IUserRepository userRepository):IRequestHandler<RefreshTokenQuery, AuthTokenResponseDto>
+{
+    public async Task<AuthTokenResponseDto> Handle(RefreshTokenQuery request, CancellationToken cancellationToken)
+    {
+        var userRefreshToken =
+            await refreshTokenRepository.GetRefreshTokenByHash(request.RefreshToken, cancellationToken);
+        if (userRefreshToken==null || userRefreshToken.ExpiresAt < DateTimeOffset.UtcNow) throw new ArgumentException("The refresh token is expired");
+        var user = await userRepository.GetByIdAsync(userRefreshToken.UserId, cancellationToken);
+        if (user == null) throw new NotFoundException(nameof(User), userRefreshToken.UserId);
+        var accessToken =  jwtTokenService.GenerateToken(user.Id, user.Email.Value);
+        string newRefreshToken = jwtTokenService.GenerateRefreshToken();
+        RefreshToken newUserRefreshToken = new RefreshToken(userRefreshToken.UserId, newRefreshToken,DateTimeOffset.UtcNow.AddDays(7));
+        await refreshTokenRepository.AddAsync(newUserRefreshToken, cancellationToken);
+        return new AuthTokenResponseDto(accessToken, newRefreshToken);
+
+
+    }
+}
---
+++ TaxRadar-Application/Queries/Auth/RefreshTokenQuery.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+using TaxRadar_Application.DTOs.Auth;
+
+namespace TaxRadar_Application.Queries.Auth;
+
+public record RefreshTokenQuery(String RefreshToken):IRequest<AuthTokenResponseDto>;
