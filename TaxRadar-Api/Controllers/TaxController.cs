90faa5539745b11bbdf2fa571569c6e37b6db86c
Oleh_Hulevych <oleggulevic8@gmail.com>
2026-09-16 18:17:03 +0200

    added endpoint to get vat registration status

--- TaxRadar-Api/Controllers/TaxController.cs
+++ TaxRadar-Api/Controllers/TaxController.cs
@@ -14,5 +14,11 @@ public class TaxController(ISender sender):ControllerBase
         var result = await sender.Send(query);
         return Ok(result);
     }
-    
+
+    [HttpGet("vat-registration-status")]
+    public async Task<IActionResult> GetVatRegistrationStatus([FromQuery] GetVatRegistrationStatusQuery query)
+    {
+        var result = await sender.Send(query);
+        return Ok(result);
+    }
 }
